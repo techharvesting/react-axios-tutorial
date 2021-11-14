e5fa31dfa85f5191264258a74eeef9dfdb98d850
Naseel Niyas <naseel@techharvesting.in>
2021-11-14 12:10:38 +0530

    🎊 Added Required Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const fetchPosts = async () => {
+    const response = await axios('http://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts.map((post, id) => (
+        <h1 key={id}>{post.title}</h1>
+      ))}
     </div>
   );
 }
